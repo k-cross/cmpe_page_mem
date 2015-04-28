c4fc98ca71c4fd32ce98789666712b1c38970690
k-cross <kenneth.cross@sjsu.edu>
2015-04-27 23:10:45 -0700

    async

--- server/tcp_boostsv.cpp
+++ server/tcp_boostsv.cpp
@@ -9,6 +9,9 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
@@ -19,22 +22,74 @@ std::string make_daytime_string(){
   return ctime(&now);
 }
 
+class tcp_connection : public boost::enable_shared_from_this<tcp_connection>{
+public:
+  typedef boost::shared_ptr<tcp_connection> pointer;
+
+  static pointer create(boost::asio::io_service& io_service){
+    return pointer(new tcp_connection(io_service));
+  }
+
+  tcp::socket& socket(){
+    return socket_;
+  }
+
+  void start(){
+    message_ = make_daytime_string();
+
+    boost::asio::async_write(socket_, boost::asio::buffer(message_),
+        boost::bind(&tcp_connection::handle_write, shared_from_this(),
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  tcp_connection(boost::asio::io_service& io_service) : socket_(io_service)
+  {
+  }
+
+  void handle_write(const boost::system::error_code& /*error*/, size_t /*bytes_transferred*/)
+  {
+  }
+
+  tcp::socket socket_;
+  std::string message_;
+};
+
+class tcp_server{
+public:
+  tcp_server(boost::asio::io_service& io_service)
+    : acceptor_(io_service, tcp::endpoint(tcp::v4(), 6666)){ //listen on port 6666
+    start_accept();
+  }
+
+private:
+  void start_accept(){
+    tcp_connection::pointer new_connection =
+      tcp_connection::create(acceptor_.get_io_service());
+
+    acceptor_.async_accept(new_connection->socket(),
+        boost::bind(&tcp_server::handle_accept, this, new_connection,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(tcp_connection::pointer new_connection,
+      const boost::system::error_code& error){
+    if (!error){
+      new_connection->start();
+    }
+
+    start_accept();
+  }
+
+  tcp::acceptor acceptor_;
+};
+
 int main(){
   try{
     boost::asio::io_service io_service;
-
-    //listens on port 6666
-    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 6666));
-
-    for (;;){
-      tcp::socket socket(io_service);
-      acceptor.accept(socket);
-
-      std::string message = make_daytime_string();
-
-      boost::system::error_code ignored_error;
-      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
-    }
+    tcp_server server(io_service);
+    io_service.run();
   }
   catch (std::exception& e){
     std::cerr << e.what() << std::endl;
