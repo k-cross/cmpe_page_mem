b37e09f1c22b3e59abe51e383136aa7370e6fbe2
k-cross <kenneth.cross@sjsu.edu>
2015-05-04 11:02:16 -0700

    silly changes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Kernel module that creates memory pages that are mapped to a networked computer.
 
 ## Compile Instructions
 * For compiling either the client or server, go to respective directory and type **make**.
-* Tested for kernel versions ...
+* Tested for kernel versions 3.16 on LMDE2
 
 ## Network Instructions
 For VirtualBox, go to **settings** in the host machine, click **network** and where there's the drop-down menu with _NAT_ as default, change it to **bridged**. This creates a network where you can see everything on your VMs network including your physical box. This makes having an internet connection easy to maintain.
--- src/kern_tests.c
+++ src/kern_tests.c
@@ -4,7 +4,7 @@
  * <Matter> Kernel Module tests
  */
 
-#include <linux/init.h>
+/*#include <linux/init.h>*/
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_INFO */
 
@@ -12,13 +12,13 @@ MODULE_LICENSE("GPL"); /* Just include this, different licenses restrict usage *
 MODULE_AUTHOR("CMPE142");
 MODULE_DESCRIPTION("Test Code");
 
-static int__init module_init(void){
+static int __init module_init(void){
     printk(KERN_INFO "Hello world 1.\n");
 
     return 0;
 }
 
-static void__exit module(void)
+static void __exit module(void)
 {
         printk(KERN_INFO "Goodbye world 1.\n");
 }
