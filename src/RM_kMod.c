3edcd0d9d32c3ec811e488f47fa675bb3b295e7e
k-cross <kenneth.cross@sjsu.edu>
2015-05-05 04:32:23 -0700

    correction

--- src/RM_kMod.c
+++ src/RM_kMod.c
@@ -28,24 +28,22 @@ back to zero in release handler*/
    #define IOCTL_PATCH_TABLE 0x00000001
    #define IOCTL_FIX_TABLE   0x00000004
 
-
 /* Pointer to the original sys_open */
 asmlinkage int (*real_open)(const char* __user, int, int);
 
 /* Our replacement */
-asmlinkage int custom_open(const char* __user file_name, int flags, int mode)
-{
+asmlinkage int custom_open(const char* __user file_name, int flags, int mode){
    printk("interceptor: open(\"%s\", %X, %X)\n", file_name,
                                                  flags,
                                                  mode);
    return real_open(file_name, flags, mode);
 }
 
-// This part is to allow us to edit the sys_call_table directly (even though it is protected)
-	/* Make the page writable */
+/* This part is to allow us to edit the sys_call_table directly (even though it is protected)
+	Make the page writable */
 int make_rw(unsigned long address){
 	unsigned int level;
-	// pte_t --> Page Table Entry
+	/* pte_t --> Page Table Entry */
 	pte_t *pte = lookup_address(address, &level);
 	if(pte->pte &~ _PAGE_RW)
 		pte->pte |= _PAGE_RW;
@@ -82,10 +80,7 @@ static int our_open(struct inode *inode, struct file *file){
  static int our_ioctl(struct file *file, unsigned int cmd, unsigned long arg){
     int retval = 0;
 
-    //Implementation of iocrl
-
-    switch(cmd)
-       {
+    switch(cmd){ /*Ioctl Implementation */
           case IOCTL_PATCH_TABLE:
              make_rw((unsigned long)sys_call_table);
              real_open = (void*)*(sys_call_table + __NR_open);
@@ -107,37 +102,32 @@ static int our_open(struct inode *inode, struct file *file){
     return retval;
  }
 
- static const struct file_operations our_fops =\
-    {
+ static const struct file_operations our_fops =\{
        .owner = THIS_MODULE,
        .open = &our_open,
        .release = &our_release,
        .unlocked_ioctl = (void*)&our_ioctl,
        .compat_ioctl = (void*)&our_ioctl
     };
- static struct miscdevice our_device = \
-    {
+
+ static struct miscdevice our_device = \{
        MISC_DYNAMIC_MINOR,
        "interceptor",
        &our_fops
     };
 
-
-static int __init init_mod(void)
-{
+static int __init init_mod(void){
 	int retval = misc_register(&our_device); // Register this device with the system
 
     printk(KERN_INFO "Remote memory Module Successfully Initialized!\n");
     return retval;    // Note: Non-zero return means that the module couldn't be loaded.
 }
 
-static void __exit cleanup_mod(void)
-{
+static void __exit cleanup_mod(void){
 	misc_deregister(&our_device); //Unregister this device with the system
 
 	// check to see if our system call is in use and if it it revert back before exiting (rmmod)
-	if(is_set)
-	   {
+	if(is_set){
 	      make_rw((unsigned long)sys_call_table);
 	      *(sys_call_table + __NR_open) = (unsigned long)real_open;
 	      make_ro((unsigned long)sys_call_table);
@@ -146,7 +136,5 @@ static void __exit cleanup_mod(void)
     printk(KERN_INFO "Remote Memory Module Is cleaning up and Exiting...\n");
 }
 
-
-// Execute init_mod and cleanup_mod upon loading and unloading the module
 module_init(init_mod);
 module_exit(cleanup_mod);
--- src/manager.c
+++ src/manager.c
@@ -2,13 +2,12 @@
 #include <sys/ioctl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
-
 
 /* Define ioctl commands */
 #define IOCTL_PATCH_TABLE 0x00000001
 #define IOCTL_FIX_TABLE   0x00000004
-
 
 int main(void)
 {
@@ -19,10 +18,12 @@ int main(void)
 	   printf("FAILED! Device Number: %i\n",device);
 	   return 0;
    }
+
    printf("Device Number: %i",device);
    ioctl(device, IOCTL_PATCH_TABLE);
    sleep(5);
    ioctl(device, IOCTL_FIX_TABLE);
    close(device);
+
    return 0;
 }
