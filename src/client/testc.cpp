c51eef242de73cfac1e3ff796c2401f9ffd293a4
k-cross <kenneth.cross@sjsu.edu>
2015-05-06 13:00:01 -0700

    testc revert

--- src/client/testc.cpp
+++ src/client/testc.cpp
@@ -2,18 +2,11 @@
 #include <string>
 #include "../tcp_api.cpp"
 
-extern "C" {
-#include "./netlinkUser.c"
-}
-
 int main(int argc, char* argv[]){
   std::string ip_addr = argv[1];
   std::string filepath = argv[2];
 
   file_send(ip_addr, filepath);
-
-  char a[25] = "this is a test";
- <char*>captains_log(a);
 
   return 0;
 }
