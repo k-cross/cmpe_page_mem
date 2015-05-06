526a0d0d56bbdcbd5e7048fd8682f6d635896e40
k-cross <kenneth.cross@sjsu.edu>
2015-05-06 03:29:13 -0700

    functional changes and some tests

---
+++ src/client/net_test.c
@@ -0,0 +1,10 @@
+#include "netlinkUser.c"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+  captains_log(argv[1]);
+
+  return(0);
+}
--- src/client/netlinkUser.c
+++ src/client/netlinkUser.c
@@ -25,44 +25,48 @@ struct msghdr msg;
  * where the only issue is figuring out how to implement
  * it with the rest of our code. */
 
-int main(){
+void captains_log(char *delivery){
+  char *retort; /* Added to save kernel response for possible output */
+
   sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
-  if(sock_fd < 0)
-    return -1;
-  
-  memset(&src_addr, 0, sizeof(src_addr));
-  src_addr.nl_family = AF_NETLINK;
-  src_addr.nl_pid = getpid(); /* self pid */
-  
-  bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
-  
-  memset(&dest_addr, 0, sizeof(dest_addr));
-  memset(&dest_addr, 0, sizeof(dest_addr));
-  dest_addr.nl_family = AF_NETLINK;
-  dest_addr.nl_pid = 0; /* For Linux Kernel */
-  dest_addr.nl_groups = 0; /* unicast */
-  
-  nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
-  memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
-  nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
-  nlh->nlmsg_pid = getpid();
-  nlh->nlmsg_flags = 0;
-  
-  strcpy(NLMSG_DATA(nlh), "Hello"); /* Sends this string to kernel module */
-  
-  iov.iov_base = (void *)nlh;
-  iov.iov_len = nlh->nlmsg_len;
-  msg.msg_name = (void *)&dest_addr;
-  msg.msg_namelen = sizeof(dest_addr);
-  msg.msg_iov = &iov;
-  msg.msg_iovlen = 1;
-  
-  printf("Sending message to kernel\n");
-  sendmsg(sock_fd,&msg,0);
-  printf("Waiting for message from kernel\n");
-  
-  /* Read message from kernel */
-  recvmsg(sock_fd, &msg, 0);
-  printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
-  close(sock_fd);
+  if(sock_fd >= 0){
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = getpid(); /* self pid */
+    
+    bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
+    
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0; /* For Linux Kernel */
+    dest_addr.nl_groups = 0; /* unicast */
+    
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = getpid();
+    nlh->nlmsg_flags = 0;
+    
+    strcpy(NLMSG_DATA(nlh), delivery); /* Sends this string to kernel module */
+    
+    iov.iov_base = (void *)nlh;
+    iov.iov_len = nlh->nlmsg_len;
+    msg.msg_name = (void *)&dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    
+    printf("Sending message to kernel\n");
+    sendmsg(sock_fd,&msg,0);
+    printf("Waiting for message from kernel\n");
+    
+    /* Read message from kernel */
+    recvmsg(sock_fd, &msg, 0);
+    retort = (char *)NLMSG_DATA(nlh);
+    printf("Received message payload: %s\n", (char *)NLMSG_DATA(nlh));
+    close(sock_fd);
+  }
+  else
+    printf("Socket Error\n");
 }
