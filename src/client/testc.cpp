192ee9b05852892fee017663152defc89c8fbdab
k-cross <kenneth.cross@sjsu.edu>
2015-05-06 03:38:32 -0700

    small test

--- src/client/testc.cpp
+++ src/client/testc.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <string>
 #include "../tcp_api.cpp"
+#include "./netlinkUser.c"
 
 int main(int argc, char* argv[]){
   std::string ip_addr = argv[1];
   std::string filepath = argv[2];
 
   file_send(ip_addr, filepath);
+
+  captains_log("this is a test");
 
   return 0;
 }
