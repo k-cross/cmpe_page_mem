6c08a00c66632e40d45e8ec59b78c00ce716a854
ZVonFeng <ziyi.feng.zoe@gmail.com>
2015-05-05 17:47:09 -0700

    Update netlinkKernel.c

--- src/client/netlinkKernel.c
+++ src/client/netlinkKernel.c
@@ -1,4 +1,3 @@
-#include <linux/kernel.h>
 #include <linux/module.h>
 #include <net/sock.h>
 #include <linux/netlink.h>
@@ -12,11 +11,11 @@ struct sock *nl_sk = NULL;
 
 static void hello_nl_recv_msg(struct sk_buff *skb) {
   struct nlmsghdr *nlh;
+  int pid;
   struct sk_buff *skb_out;
   int msg_size;
-  int pid;
-  int res;
   char *msg="Hello from kernel";
+  int res;
   
   printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
   
@@ -24,7 +23,7 @@ static void hello_nl_recv_msg(struct sk_buff *skb) {
   
   nlh=(struct nlmsghdr*)skb->data;
   printk(KERN_INFO "Netlink received msg payload:%s\n",(char*)nlmsg_data(nlh));
-  pid = nlh->nlmsg_pid; /*pid of sending process */
+  pid = nlh->nlmsg_pid; //pid of sending process 
   
   skb_out = nlmsg_new(msg_size,0);
   
@@ -34,16 +33,20 @@ static void hello_nl_recv_msg(struct sk_buff *skb) {
   } 
 
   nlh=nlmsg_put(skb_out,0,0,NLMSG_DONE,msg_size,0);  
-  NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
+  NETLINK_CB(skb_out).dst_group = 0; //not in mcast group
   strncpy(nlmsg_data(nlh),msg,msg_size);
-  res = nlmsg_unicast(nl_sk,skb_out,pid);
+  res=nlmsg_unicast(nl_sk,skb_out,pid);
+
+  if(res<hello_nl_recv_msg)
+      printk(KERN_INFO "Error while sending bak to user\n");
 }
 
 static int __init hello_init(void) {
   printk("Entering: %s\n",__FUNCTION__);
-  /* This is for 3.6 kernels and above. */
   
-  struct netlink_kernel_cfg cfg = {.input = hello_nl_recv_msg,};
+  struct netlink_kernel_cfg cfg = {
+      .input = hello_nl_recv_msg,
+  };
 
   nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &cfg);
   
